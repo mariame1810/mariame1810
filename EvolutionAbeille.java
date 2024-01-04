01c0b72df54422f005e278457b94c25817b070d6
mariame1810 <155655524+mariame1810@users.noreply.github.com>
2024-01-04 16:48:46 +0100

    Create EvolutionAbeille.java
    This is the interface which transform a type of bee to another

---
+++ EvolutionAbeille.java
@@ -0,0 +1,4 @@
+public interface EvolutionAbeille {
+	// méthode permettant à l'abeille d'évoluer d'une simple Abeille à une abeille Butineuse qui a sont tour se transforme en abeille Magasinière.
+	public boolean transformationAbeille();
+}
