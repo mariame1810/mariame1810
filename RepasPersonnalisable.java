525fe17f0a8acacfa289e0fe1c3aafb0a791ea32
mariame1810 <155655524+mariame1810@users.noreply.github.com>
2024-01-04 17:04:06 +0100

    Add files via upload

---
+++ RepasPersonnalisable.java
@@ -0,0 +1,12 @@
+
+
+/**
+ * Inerface de personnalisation d'un composant de menu
+ */
+
+public interface RepasPersonnalisable {
+	
+	// mthode permettant de retirer un ingredient de la recette
+    public void retirerIngredient();
+    public void compoNutri();
+}
